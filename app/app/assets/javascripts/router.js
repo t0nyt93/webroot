dfb1330413c1822d5bafe32599e0171c97b6b858
Tony <tonytyrrell93@gmail.com>
2017-01-14 16:28:01 -0800

    Working multi ui-views

--- app/app/assets/javascripts/router.js
+++ app/app/assets/javascripts/router.js
@@ -1,41 +1,53 @@
 angular.module('routerApp', ['ui.router', 'templates']).
     config([
-    '$stateProvider',
-    '$urlRouterProvider',
-    '$locationProvider',
-    function($stateProvider, $urlRouterProvider, $locationProvider){
-        $stateProvider.state('projects', {
-            url: '/projects',
-            templateUrl: 'projects.html',
-            controller: 'ProjectsCtrl'
-        });
-        $stateProvider.state('home', {
-            url: '/home',
-            templateUrl: 'home.html',
-            controller: 'HomeCtrl'
-        });
-        $stateProvider.state('about', {
-            url: '/about',
-            templateUrl: 'about.html',
-            controller: 'AboutCtrl'
-        });
-        $stateProvider.state('contact', {
-            url: '/contact',
-            templateUrl: 'contact.html',
-            controller: 'ContactCtrl'
-        });
-        $stateProvider.state('webgl', {
-            url: '/webgl',
-            templateUrl: 'webgl.html',
-            controller: 'WebglCtrl'
-        });
+        '$stateProvider',
+        '$urlRouterProvider',
+        '$locationProvider',
+        function($stateProvider, $urlRouterProvider, $locationProvider){
+            $stateProvider
+                .state('projects', {
+                    url: '/projects',
+                    templateUrl: 'projects.html',
+                    controller: 'ProjectsCtrl'
+                })
+                .state('projects.detail', {
+                    url: '/detail/:id',
+                    templateUrl: 'project_detail.html',
+                    controller: 'ProjectDetailCtrl'
+                })
+                .state('home', {
+                    url: '/home',
+                    views: {
+                        '': {templateUrl : 'home/home.html' },
+                        'nav@home':{ templateUrl : 'partials/navbar.html'},
+                        'body@home':{
+                            templateUrl : 'home/home.html',
+                            controller: 'HomeCtrl'
+                        },
+                        'footer@home':{ templateUrl : 'partials/footer.html' },
 
+                    }
+                })
+                .state('about', {
+                    url: '/about',
+                    templateUrl: 'about.html',
+                    controller: 'AboutCtrl'
+                })
+                .state('contact', {
+                    url: '/contact',
+                    templateUrl: 'contact.html',
+                    controller: 'ContactCtrl'
+                })
+                .state('webgl', {
+                    url: '/webgl',
+                    templateUrl: 'webgl.html',
+                    controller: 'WebglCtrl'
+                });
 
+            $urlRouterProvider.otherwise('home');
 
-        $urlRouterProvider.otherwise('home');
-
-        $locationProvider.html5Mode({
-            enabled:true,
-            requireBase: false
-        });
-    }]);
+            $locationProvider.html5Mode({
+                enabled:true,
+                requireBase: false
+            });
+        }]);
